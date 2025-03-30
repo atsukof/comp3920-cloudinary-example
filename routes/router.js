bc851993a89e6bf3666f84d101f97ce143af8cb5
Atsuko Uemura <atsukof.kt@gmail.com>
2025-03-29 18:44:46 -0700

    Add uploading image functionality in index.ejs

--- routes/router.js
+++ routes/router.js
@@ -37,7 +37,7 @@ router.get('/', async (req, res) => {
 
 
 	try {
-		const users = await userCollection.find().project({first_name: 1, last_name: 1, email: 1, _id: 1}).toArray();
+		const users = await userCollection.find().project({first_name: 1, last_name: 1, email: 1, _id: 1, image_id: 1}).toArray();
 
 		if (users === null) {
 			res.render('error', {message: 'Error connecting to MongoDB'});
@@ -129,6 +129,53 @@ router.post('/setPetPic', upload.single('image'), function(req, res, next) {
 				console.log(ex);
 			}
 		}, 
+		{ public_id: image_uuid }
+	);
+	console.log(req.body);
+	console.log(req.file);
+});
+
+router.post('/setUserPic', upload.single('image'), function (req, res, next) {
+	let image_uuid = uuid();
+	let user_id = req.body.user_id;
+	let buf64 = req.file.buffer.toString('base64');
+	stream = cloudinary.uploader.upload("data:image/octet-stream;base64," + buf64, async function (result) {
+		try {
+			console.log(result);
+			console.log("userId: " + user_id);
+
+			// Joi validate
+			const schema = Joi.object(
+				{
+					user_id: Joi.string().alphanum().min(24).max(24).required()
+				});
+
+			const validationResult = schema.validate({ user_id });
+			if (validationResult.error != null) {
+				console.log(validationResult.error);
+
+				res.render('error', { message: 'Invalid user_id' });
+				return;
+			}
+			const success = await userCollection.updateOne({ "_id": new ObjectId(user_id) },
+				{ $set: { image_id: image_uuid } },
+				{}
+			);
+
+			if (!success) {
+				res.render('error', { message: 'Error uploading user image to MongoDB' });
+				console.log("Error uploading user image");
+			}
+			else {
+				res.redirect('/');
+			}
+		}
+		catch (ex) {
+			res.render('error', { message: 'Error connecting to MongoDB' });
+			console.log("Error connecting to MongoDB");
+			console.log(ex);
+		}
+	},
 		{ public_id: image_uuid }
 	);
 	console.log(req.body);
@@ -253,6 +300,49 @@ router.get('/deletePetImage', async (req, res) => {
 		res.render('error', {message: 'Error connecting to MySQL'});
 		console.log("Error connecting to MySQL");
 		console.log(ex);	
+	}
+});
+
+router.get('/deleteUserImage', async (req, res) => {
+	try {
+		console.log("delete user image");
+
+		let user_id = req.query.id;
+
+		const schema = Joi.object(
+			{
+				user_id: Joi.string().alphanum().min(24).max(24).required()
+			});
+
+		const validationResult = schema.validate({ user_id });
+
+		if (validationResult.error != null) {
+			console.log(validationResult.error);
+
+			res.render('error', { message: 'Invalid user_id'});
+			return;
+		}
+
+		if (user_id) {
+			console.log("userId: " + user_id);
+			const success = await userCollection.updateOne({ "_id": new ObjectId(user_id) },
+				{ $set: { image_id: undefined } },
+				{}
+			);
+
+			console.log("delete User Image: ");
+			console.log(success);
+			if (!success) {
+				res.render('error', { message: 'Error connecting to MySQL' });
+				return;
+			}
+		}
+		res.redirect('/');
+	}
+	catch (ex) {
+		res.render('error', { message: 'Error connecting to MySQL' });
+		console.log("Error connecting to MySQL");
+		console.log(ex);
 	}
 });
 
